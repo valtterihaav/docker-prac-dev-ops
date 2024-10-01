6f995f9f1edd6349118bce9752fd3ecc5fc7b617
Valtteri <vade.haavisto@gmail.com>
2024-10-01 20:36:13 +0300

    working mostly

--- service1/app.py
+++ service1/app.py
@@ -4,6 +4,7 @@ Some basic api for subprocess info
 
 import subprocess
 from fastapi import FastAPI
+import requests
 
 # init app
 app = FastAPI()
@@ -11,7 +12,7 @@ app = FastAPI()
 
 # root endpoint
 @app.get("/")
-def read_root():
+async def read_root():
     """
     Root endpoint.
     """
@@ -20,9 +21,22 @@ def read_root():
     disk_space = subprocess.getoutput("df -h /")
     uptime = subprocess.getoutput("uptime -p")
 
+    # query the second api
+    api_url = "http://localhost:8200/"
+
+    try:
+        response = requests.get(api_url, timeout=10)
+        response = response.json()
+
+    except requests.exceptions.RequestException as e:
+        response = {"error": str(e)}
+
     return {
-        "ip_address": ip_address,
-        "processes": processes,
-        "disk_space": disk_space,
-        "uptime": uptime,
+        "Service1": {
+            "ip_address": ip_address,
+            "processes": processes,
+            "disk_space": disk_space,
+            "uptime": uptime,
+        },
+        "Service2": response,
     }
